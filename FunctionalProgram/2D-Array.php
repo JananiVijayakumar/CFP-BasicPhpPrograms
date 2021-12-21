ac9ff1205c08f8e65e9fc51d4e59949843933f92
Janani <vsvprema@gmail.com>
2021-12-21 11:49:30 +0530

    Added: Functional programs

---
+++ FunctionalProgram/2D-Array.php
@@ -0,0 +1,8 @@
+<?php
+$novels = array (
+'tamil' =>array ("Ponniyin selvan", "Kalki", "5"),
+'English' =>array ("Harry Porter", "J. K. Rowling", "8"),
+'Telungu' =>array ("Amuktamalyada", "Krishnadevaraya", "3")
+);
+echo "Tamil Novel Author : ".$novels['tamil'][1];
+?>
