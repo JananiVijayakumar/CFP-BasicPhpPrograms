b72b942643a25a949c593f5c003537a326a6ecd6
Janani <vsvprema@gmail.com>
2021-12-21 11:03:06 +0530

    Added : Basic core programs

---
+++ BasicCorePrograms/LargestNumber.php
@@ -0,0 +1,12 @@
+<?php
+$number1 = readline('Enter number 1 :');
+$number2 = readline('Enter number 2 :');
+$number3 = readline('Enter number 3 :');
+if(($number1>$number2) & ($number1>$number3)){
+    echo "$number1 is the Largest";
+}elseif(($number2>$number1) & ($number2>$number3)){
+    echo "$number2 is the Largest";
+}else {
+    echo "$number3 is the Largest";
+}
+?>
