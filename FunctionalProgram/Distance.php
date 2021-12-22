f8926d7ae0f8092fb492108502eff3a4affb5f87
Janani <vsvprema@gmail.com>
2021-12-22 10:00:32 +0530

    Added : Logical programs are added

--- FunctionalProgram/Distance.php
+++ FunctionalProgram/Distance.php
@@ -1,5 +1,8 @@
 <?php
 $x = readline ('Enter x value :');
 $y = readline ('Enter y value :');
-$distance = sqrt (($x*$x)+($y*$y));
-echo "The distance of x and y is :".$distance;
+function distance($x,$y){
+    echo "distance Of 2 Integer =. sqrt (($x*$x)+($y*$y));"
+}
+echo "The distance of x and y is :".distance();
+?>
